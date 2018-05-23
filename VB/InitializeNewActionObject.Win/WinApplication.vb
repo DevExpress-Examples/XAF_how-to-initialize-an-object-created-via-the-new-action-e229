684778f260ff882c01d9e982d1853293ad107cf1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ VB/InitializeNewActionObject.Win/WinApplication.vb
@@ -0,0 +1,21 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports DevExpress.ExpressApp.Win
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.ExpressApp
+
+Namespace HowToInitializeObjectCreatedViaNewAction.Win
+	Partial Public Class HowToInitializeObjectCreatedViaNewActionWindowsFormsApplication
+		Inherits WinApplication
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub HowToInitializeObjectCreatedViaNewActionWindowsFormsApplication_DatabaseVersionMismatch(ByVal sender As Object, ByVal e As DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs) Handles MyBase.DatabaseVersionMismatch
+			e.Updater.Update()
+			e.Handled = True
+		End Sub
+	End Class
+End Namespace
