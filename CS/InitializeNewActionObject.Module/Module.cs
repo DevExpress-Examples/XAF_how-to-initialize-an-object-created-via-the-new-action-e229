7ea37b806a33636016c69d62b60487834873446d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ CS/InitializeNewActionObject.Module/Module.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+using System.Reflection;
+
+
+namespace HowToInitializeObjectCreatedViaNewAction.Module {
+    public sealed partial class HowToInitializeObjectCreatedViaNewActionModule : ModuleBase {
+        public HowToInitializeObjectCreatedViaNewActionModule() {
+            InitializeComponent();
+        }
+    }
+}
